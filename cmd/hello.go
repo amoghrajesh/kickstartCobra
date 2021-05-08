d2a4087481d8f02e04db5b77f2aaba169f09e8a2
Amogh <adesai@cloudera.com>
2021-05-08 12:49:14 +0530

    Removing uneccessary comments to test the workflow

--- cmd/hello.go
+++ cmd/hello.go
@@ -65,13 +65,4 @@ func addFloats(args []string) {
 func init() {
 	rootCmd.AddCommand(helloCmd)
 	helloCmd.Flags().BoolP("isfloat", "f", false, "Are the numbers float?")
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// helloCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// helloCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
